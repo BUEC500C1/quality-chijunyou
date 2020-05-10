9512a4bd24db36c6e70e3d1cd6a9e9743046de01
chijunyou <55154341+chijunyou@users.noreply.github.com>
2020-05-10 14:50:02 -0400

    upload test

---
+++ hw1_test.py
@@ -0,0 +1,18 @@
+from A2R import ArabicToRoman
+import pytest
+def testcase():
+	assert ArabicToRoman(1) == "I"
+	assert ArabicToRoman(7) == ""
+	assert ArabicToRoman(10) == "X"
+	assert ArabicToRoman(12) == "XII"
+	assert ArabicToRoman(19) == "XIX"
+	assert ArabicToRoman(105) == "CV"
+	assert ArabicToRoman(137) == "CXXXVII"
+	assert ArabicToRoman(777) == "DCCLXXVII"
+	assert ArabicToRoman(1390) == "MCCCXC"
+	assert ArabicToRoman(2799) == "MMDCCXCIX"
+	assert ArabicToRoman(4000) == -1
+	assert ArabicToRoman(0) == -1
+	assert ArabicToRoman(-1) == -1
+	assert ArabicToRoman('a') == -2
+
